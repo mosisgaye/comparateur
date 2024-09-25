cd6f4a2d9db5828ef16cf9092f83934c8e3673fb
Moussa <mosisg7@gmail.com>
2024-09-25 14:19:05 +0100

    Description des modifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : "http://compareprix.net";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
